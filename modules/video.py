69bcfbc56e0ab6e9712991e12855a6692dbde6db
TEAM Alpha <89734618+Adarshbotmaker@users.noreply.github.com>
2021-11-19 19:29:21 +0530

    Update video.py

--- modules/video.py
+++ modules/video.py
@@ -16,8 +16,8 @@ from youtube_dl.utils import (
     XAttrMetadataError,
 )
 
-from Natsuki.events import register
-from Natsuki.utils import progress
+from Alphax.events import register
+from Alphax.utils import progress
 
 try:
     from youtubesearchpython import SearchVideos
