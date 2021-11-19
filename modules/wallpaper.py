a1632a07e99820aa111d455a0e3e0804689337b5
TEAM Alpha <89734618+Adarshbotmaker@users.noreply.github.com>
2021-11-19 19:28:32 +0530

    Update wallpaper.py

--- modules/wallpaper.py
+++ modules/wallpaper.py
@@ -4,8 +4,8 @@ import requests as r
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
-from Natsuki import SUPPORT_CHAT, WALL_API, dispatcher
-from Natsuki.modules.disable import DisableAbleCommandHandler
+from Alphax import SUPPORT_CHAT, WALL_API, dispatcher
+from Alphax.modules.disable import DisableAbleCommandHandler
 
 # Wallpapers module by @TheRealPhoenix using wall.alphacoders.com
 
