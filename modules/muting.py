b08cd15de0e21a3dfa060d43b96882345c6d1af2
UTKARSHBOTS <90956407+utkarsh212646@users.noreply.github.com>
2021-11-19 20:13:26 +0530

    Update muting.py

--- modules/muting.py
+++ modules/muting.py
@@ -5,20 +5,20 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
 
-from Natsuki import LOGGER, TIGERS, dispatcher
-from Natsuki.modules.helper_funcs.chat_status import (
+from Alphax import LOGGER, TIGERS, dispatcher
+from Alphax.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
     is_user_admin,
     user_admin,
 )
-from Natsuki.modules.helper_funcs.extraction import (
+from Alphax.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from Natsuki.modules.helper_funcs.string_handling import extract_time
-from Natsuki.modules.log_channel import loggable
+from Alphax.modules.helper_funcs.string_handling import extract_time
+from Alphax.modules.log_channel import loggable
 
 
 @run_async
