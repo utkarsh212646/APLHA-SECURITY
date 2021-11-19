aac64f5a382585c6102a9dbe3ff2349b6d9857b3
UTKARSHBOTS <90956407+utkarsh212646@users.noreply.github.com>
2021-11-19 19:30:59 +0530

    Update dev.py

--- modules/dev.py
+++ modules/dev.py
@@ -6,8 +6,8 @@ from time import sleep
 from telegram import TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from Natsuki import dispatcher
-from Natsuki.modules.helper_funcs.chat_status import dev_plus
+from Alphax import dispatcher
+from Alphax.modules.helper_funcs.chat_status import dev_plus
 
 
 @run_async
