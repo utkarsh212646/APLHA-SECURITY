fc7107cdbde2047c2b498556b0e9d9a027245f5c
TEAM Alpha <89734618+Adarshbotmaker@users.noreply.github.com>
2021-11-19 19:17:30 +0530

    Update wiki.py

--- modules/wiki.py
+++ modules/wiki.py
@@ -3,8 +3,8 @@ from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
 
-from Natsuki import dispatcher
-from Natsuki.modules.disable import DisableAbleCommandHandler
+from Alpha import dispatcher
+from Alphax.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
