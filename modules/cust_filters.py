f7c3e93a7b2cda65c42f88ea2973184ecf2af10a
UTKARSHBOTS <90956407+utkarsh212646@users.noreply.github.com>
2021-11-19 19:26:39 +0530

    Update cust_filters.py

--- modules/cust_filters.py
+++ modules/cust_filters.py
@@ -15,23 +15,23 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from Natsuki import DRAGONS, LOGGER, dispatcher
-from Natsuki.modules.connection import connected
-from Natsuki.modules.disable import DisableAbleCommandHandler
-from Natsuki.modules.helper_funcs.alternate import send_message, typing_action
-from Natsuki.modules.helper_funcs.chat_status import user_admin
-from Natsuki.modules.helper_funcs.extraction import extract_text
-from Natsuki.modules.helper_funcs.filters import CustomFilters
-from Natsuki.modules.helper_funcs.handlers import MessageHandlerChecker
-from Natsuki.modules.helper_funcs.misc import build_keyboard_parser
-from Natsuki.modules.helper_funcs.msg_types import get_filter_type
-from Natsuki.modules.helper_funcs.string_handling import (
+from Alphax import DRAGONS, LOGGER, dispatcher
+from Alphax.modules.connection import connected
+from Alphax.modules.disable import DisableAbleCommandHandler
+from Alphax.modules.helper_funcs.alternate import send_message, typing_action
+from Alphax.modules.helper_funcs.chat_status import user_admin
+from Alphax.modules.helper_funcs.extraction import extract_text
+from Alphax.modules.helper_funcs.filters import CustomFilters
+from Alphax.modules.helper_funcs.handlers import MessageHandlerChecker
+from Alphax.modules.helper_funcs.misc import build_keyboard_parser
+from Alphax.modules.helper_funcs.msg_types import get_filter_type
+from Alphax.modules.helper_funcs.string_handling import (
     button_markdown_parser,
     escape_invalid_curly_brackets,
     markdown_to_html,
     split_quotes,
 )
-from Natsuki.modules.sql import cust_filters_sql as sql
+from Alphax.modules.sql import cust_filters_sql as sql
 
 HANDLER_GROUP = 10
 
