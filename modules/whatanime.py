283d83f60af94017ba059e66713668d23537511c
UTKARSHBOTS <90956407+utkarsh212646@users.noreply.github.com>
2021-11-19 20:44:06 +0530

    Update whatanime.py

--- modules/whatanime.py
+++ modules/whatanime.py
@@ -12,7 +12,7 @@ import aiohttp
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from Natsuki import pbot
+from Alphax import pbot
 
 session = aiohttp.ClientSession()
 progress_callback_data = {}
